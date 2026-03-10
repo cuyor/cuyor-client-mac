bda42557e32ae8f2d4991f3914b2e870e205b936
Umar Ahmed <39865635+UmarGit@users.noreply.github.com>
2026-03-10 18:41:45 +0300

    Initial Commit

---
+++ cuyor/cuyorApp.swift
@@ -0,0 +1,17 @@
+//
+//  cuyorApp.swift
+//  cuyor
+//
+//  Created by Umar Ahmed on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct cuyorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
